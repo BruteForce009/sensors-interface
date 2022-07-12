1773fa8d8a65320fb15aae8107725ca414b3bca8
BruteForce009 <1517satyamtomar@gmail.com>
2022-07-13 00:46:46 +0530

    Added RTC Time

--- appdir/models.py
+++ appdir/models.py
@@ -19,33 +19,12 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(45), unique=True, nullable=False)
     profile_pic = db.Column(db.String(45), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    sensors = db.relationship('Sensor', backref='user', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
 
 
-class SensorTypes(enum.Enum):
-    TEMPERATURE = 'temperature'
-    HUMIDITY = 'humidity'
-    PRESSURE = 'pressure'
-    LUMINOSITY = 'luminosity'
-
-
-class Sensor(db.Model, UserMixin):
-    model_no = db.Column(db.String(30), nullable=False, primary_key=True)
-    type = db.Column(db.Enum(SensorTypes), nullable=False)
-    desc = db.Column(db.String(100), nullable=True)
-    latitude = db.Column(db.Float(precision=6), nullable=True)
-    longitude = db.Column(db.Float(precision=6), nullable=True)
-    value = db.Column(db.Float(precision=6), nullable=True)
-    owner = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"{self.model_no} {self.type} {self.latitude} {self.longitude} {self.value}"
-
-
-class SensorData(db.Model):
+class SensorDataUno(db.Model):
     NodeID = db.Column(db.String(30), nullable=True, unique=False)
     pm1 = db.Column(db.Float(precision=2), nullable=True, unique=False)
     pm2 = db.Column(db.Float(precision=2), nullable=True, unique=False)
@@ -64,3 +43,49 @@ class SensorData(db.Model):
 
     def __repr__(self):
         return f"{self.NodeID} {self.pm1} {self.pm2} {self.pm3} {self.am} {self.twd} {self.sm1} {self.sm2} {self.st} {self.lum} {self.temp} {self.humd} {self.pres} {self.bat} {self.ttime}"
+
+
+class SensorDataNrf(db.Model):
+    NodeID = db.Column(db.String(30), nullable=True, unique=False)
+    pm1 = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    pm2 = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    pm3 = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    am = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    twd = db.Column(db.String(30), nullable=True, unique=False)
+    sm = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    st = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    lum = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    temp = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    humd = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    pres = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    bat = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    ttime = db.Column(db.String(30), primary_key=True, default=remove(f'{datetime.utcnow()}'))
+
+    def __repr__(self):
+        return f"{self.NodeID} {self.pm1} {self.pm2} {self.pm3} {self.am} {self.twd} {self.sm} {self.st} {self.lum} {self.temp} {self.humd} {self.pres} {self.bat} {self.ttime}"
+
+
+class SensorData(db.Model):
+    NodeID = db.Column(db.String(30), nullable=True, unique=False)
+    pm1 = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    pm2 = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    pm3 = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    am = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    twd = db.Column(db.String(30), nullable=True, unique=False)
+    sm = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    st = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    lum = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    temp = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    humd = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    pres = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    lat = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    NSI = db.Column(db.String(30), nullable=True, unique=False)
+    long = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    EWI = db.Column(db.String(30), nullable=True, unique=False)
+    alt = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    bat = db.Column(db.Float(precision=2), nullable=True, unique=False)
+    ttime = db.Column(db.String(30), primary_key=True, default=remove(f'{datetime.utcnow()}'))
+    rtctime = db.Column(db.Integer, nullable=True, unique=False, default=404)
+
+    def __repr__(self):
+        return f"{self.NodeID} {self.pm1} {self.pm2} {self.pm3} {self.am} {self.twd} {self.sm} {self.st} {self.lum} {self.temp} {self.humd} {self.pres} {self.lat} {self.NSI} {self.long} {self.EWI} {self.alt} {self.bat} {self.ttime}"
--- appdir/routes.py
+++ appdir/routes.py
@@ -12,8 +12,27 @@ import json
 import secrets
 
 
+def save_pic(form_pic):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_pic.filename)
+    pic_fn = random_hex + f_ext
+    pic_path = os.path.join(app.root_path, 'static/images', pic_fn)
+
+    output_size=(55, 65)
+    i = Image.open(form_pic)
+    i.thumbnail(output_size)
+    i.save(pic_path)
+
+    return pic_fn
+
+
 def remove(string):
     return string.replace(" ", "|")
+
+
+def reformat(string):
+    newstring = string.replace("|", "").replace(":", "").replace("-", "")
+    return int(newstring)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -57,20 +76,6 @@ def logout():
     return redirect(url_for('login'))
 
 
-def save_pic(form_pic):
-    random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_pic.filename)
-    pic_fn = random_hex + f_ext
-    pic_path = os.path.join(app.root_path, 'static/images', pic_fn)
-
-    output_size=(55, 65)
-    i = Image.open(form_pic)
-    i.thumbnail(output_size)
-    i.save(pic_path)
-
-    return pic_fn
-
-
 @app.route("/jsondata", methods=['POST'])
 def jsondata():
     request_data = request.get_json()
@@ -81,17 +86,25 @@ def jsondata():
     pm3 = float(request_data['pm3'])
     am = float(request_data['am'])
     twd = request_data['twd']
-    sm1 = float(request_data['sm1'])
-    sm2 = float(request_data['sm2'])
+    sm = float(request_data['sm'])
     st = float(request_data['st'])
     lum = float(request_data['lum'])
     temp = float(request_data['temp'])
     humd = float(request_data['humd'])
     pres = float(request_data['pres'])
+    lat = float(request_data['lat'])
+    NSI = request_data['NSI']
+    long = float(request_data['long'])
+    EWI = request_data['EWI']
+    alt = float(request_data['alt'])
     bat = float(request_data['bat'])
-    ttime = request_data['ttime']
+    ttime = int(request_data['ttime'])
 
-    sensordata = appdir.models.SensorData(NodeID=NodeID, pm1=pm1, pm2=pm2, pm3=pm3, am=am, twd=twd, sm1=sm1, sm2=sm2, st=st, lum=lum, temp=temp, humd=humd, pres=pres, bat=bat, ttime=ttime)
+    rtctime = ttime
+    new_time = datetime.fromtimestamp(ttime)
+    ttime = remove(f'{new_time}')
+
+    sensordata = appdir.models.SensorData(NodeID=NodeID, pm1=pm1, pm2=pm2, pm3=pm3, am=am, twd=twd, sm=sm, st=st, lum=lum, temp=temp, humd=humd, pres=pres, lat=lat, NSI=NSI, long=long, EWI=EWI, alt=alt, bat=bat, ttime=ttime, rtctime=rtctime)
     db.session.add(sensordata)
     db.session.commit()
     return 'Done'
@@ -100,6 +113,12 @@ def jsondata():
 @app.route("/newjsondata", methods=['POST'])
 def newjsondata():
     request_data = request.get_json()
+
+    lat = 1.23
+    NSI = 'X'
+    long = 1.23
+    EWI = 'X'
+    alt = 1.23
     ttime = remove(f'{datetime.utcnow()}')
 
     if request_data:
@@ -115,10 +134,8 @@ def newjsondata():
             am = float(request_data['am'])
         if 'twd' in request_data:
             twd = request_data['twd']
-        if 'sm1' in request_data:
-            sm1 = float(request_data['sm1'])
-        if 'sm2' in request_data:
-            sm2 = float(request_data['sm2'])
+        if 'sm' in request_data:
+            sm = float(request_data['sm'])
         if 'st' in request_data:
             st = float(request_data['st'])
         if 'lum' in request_data:
@@ -129,49 +146,58 @@ def newjsondata():
             humd = float(request_data['humd'])
         if 'pres' in request_data:
             pres = float(request_data['pres'])
+        if 'lat' in request_data:
+            lat = float(request_data['lat'])
+        if 'NSI' in request_data:
+            NSI = request_data['NSI']
+        if 'long' in request_data:
+            long = float(request_data['long'])
+        if 'EWI' in request_data:
+            EWI = request_data['EWI']
+        if 'alt' in request_data:
+            alt = float(request_data['alt'])
         if 'bat' in request_data:
             bat = float(request_data['bat'])
         if 'ttime' in request_data:
-            ttime = request_data['ttime']
+            ttime = int(request_data['ttime'])
+            rtctime = ttime
+            new_time = datetime.fromtimestamp(ttime)
+            ttime = remove(f'{new_time}')
 
-        sensordata = appdir.models.SensorData(NodeID=NodeID, pm1=pm1, pm2=pm2, pm3=pm3, am=am, twd=twd, sm1=sm1, sm2=sm2, st=st, lum=lum, temp=temp, humd=humd, pres=pres, bat=bat, ttime=ttime)
+        sensordata = appdir.models.SensorData(NodeID=NodeID, pm1=pm1, pm2=pm2, pm3=pm3, am=am, twd=twd, sm=sm, st=st, lum=lum, temp=temp, humd=humd, pres=pres, lat=lat, NSI=NSI, long=long, EWI=EWI, alt=alt, bat=bat, ttime=ttime, rtctime=rtctime)
         db.session.add(sensordata)
         db.session.commit()
     return 'Done'
-
-
-@app.route("/data_")
-@login_required
-def data_():
-    img_file = url_for('static', filename='images/' + current_user.profile_pic)
-    # form = appdir.forms.PictureForm()
-    # if form.validate_on_submit():
-    #     pic_file = save_pic(form.picture.data)
-    #     current_user.profile_pic = pic_file
-    page = request.args.get('page', 1, type=int)
-    sensorL = appdir.models.SensorData.query.order_by(appdir.models.SensorData.ttime.desc()).paginate(page=page, per_page=9)
-    ttl = math.floor(sensorL.total/9 + 1)
-    return render_template('data_.html', sensorL=sensorL, ttl=ttl, img_file=img_file)
 
 
 @app.route("/account")
 @login_required
 def account():
     img_file = url_for('static', filename='images/'+current_user.profile_pic)
-    # form = appdir.forms.PictureForm()
-    # if form.validate_on_submit():
-    #     pic_file = save_pic(form.picture.data)
-    #     current_user.profile_pic = pic_file
-    sensor = appdir.models.Sensor.query.filter_by(owner=current_user.id).all()
-    return render_template('account.html', sensor=sensor, img_file=img_file)
+    page = request.args.get('page', 1, type=int)
+    sensorA = appdir.models.SensorDataUno.query.filter_by(NodeID=current_user.username).order_by(appdir.models.SensorDataUno.ttime.desc()).paginate(page=page, per_page=9)
+    ttl = math.floor(sensorA.total / 9 + 1)
+    return render_template('account.html', sensorA=sensorA, ttl=ttl, img_file=img_file)
 
 
 @app.route("/nrf5")
 @login_required
 def nrf5():
     img_file = url_for('static', filename='images/'+current_user.profile_pic)
-    sensor = appdir.models.Sensor.query.filter_by(owner=current_user.id).all()
-    return render_template('nrf5.html', sensor=sensor, img_file=img_file)
+    page = request.args.get('page', 1, type=int)
+    sensorN = appdir.models.SensorDataNrf.query.filter_by(NodeID=current_user.username).order_by(appdir.models.SensorDataNrf.time.desc()).paginate(page=page, per_page=9)
+    ttl = math.floor(sensorN.total / 9 + 1)
+    return render_template('nrf5.html', sensorN=sensorN, ttl=ttl, img_file=img_file)
+
+
+@app.route("/data_")
+@login_required
+def data_():
+    img_file = url_for('static', filename='images/' + current_user.profile_pic)
+    page = request.args.get('page', 1, type=int)
+    sensorL = appdir.models.SensorData.query.order_by(appdir.models.SensorData.rtctime.desc()).filter_by(NodeID=current_user.username).paginate(page=page, per_page=9)
+    ttl = math.floor(sensorL.total/9 + 1)
+    return render_template('data_.html', sensorL=sensorL, ttl=ttl, img_file=img_file)
 
 
 @app.route("/plot")
